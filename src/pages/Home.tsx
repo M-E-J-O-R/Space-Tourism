b6926fa19a14ae10d32324c7c88785cf6ebcfd96
Mejor <amujobabatunde7@gmail.com>
2022-10-07 05:31:29 +0100

    destinatio tablet mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,11 @@ export default function Home(): JSX.Element {
                 <NavBar/>
 
                 <div className="space">
-                    <div className="space__one">
+                    <article className="space__one">
                         <p className='space__one_ques'>SO, YOU WANT TO TRAVEL TO </p>
                         <p className='space__one_head'>SPACE</p>
                         <p className='space__one_talks'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-                    </div>
+                    </article>
 
                     <div className="space__two">
                         EXPLORE
