3508531c856ba2d51ccf3866f6f7dbca34906f21
Mejor <amujobabatunde7@gmail.com>
2022-10-10 10:20:54 +0100

    crew changes

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -1,12 +1,41 @@
 import NavBar from "../components/NavBar";
-import '../styles/crew.css'
+import '../styles/crew.css';
+import crew from '../assets/crew/image-douglas-hurley.webp';
 
-const Crew = ()=> {
+const Crew = () => {
     return (<>
         <div className="Crew">
-            
-            <NavBar/>
-            
+            <NavBar />
+
+            <main className="crew-container">
+                <section className="crew-details">
+                    <p className="crew-heading" > <b>02</b> MEET YOUR CREW</p>
+
+                    <article>
+
+
+                        <div className="crew-name">
+                            <p className="role">Commander</p>
+                            <p className="name">DOUGLAS HURLEY</p>
+
+                        </div>
+
+                        <p className="crew-member-info">  Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+                            and former NASA astsronaut. He launched into space for the third time as
+                            commander of Crew Dragon Demo-2.
+                        </p>
+                    </article>
+                </section>
+
+                <section className="crew-image-container">
+                    <img src={crew} alt="" className="crew-image" />
+
+                </section>
+
+
+            </main>
+
+
         </div>
     </>);
 };
