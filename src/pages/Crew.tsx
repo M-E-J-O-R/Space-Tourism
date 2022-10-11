678df0a3ff8acf3fd2c6aa30a286fa05b7350ca4
Mejor <amujobabatunde7@gmail.com>
2022-10-11 18:49:08 +0100

    done with the crew tablet mode and i also adjusted the navBar

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -28,39 +28,36 @@ const Crew = () => {
     };
     let sortedData = crewData.filter(handleRole);
 
-    let crew = sortedData[0]
-    
-    
+    let crew = sortedData[0];
+
+
 
     return (<>
         <div className="Crew">
             <NavBar />
+            <p className="crew-heading" > <b>02 </b> MEET YOUR CREW</p>
 
             <main className="crew-container">
                 <section className="crew-details">
-                    <p className="crew-heading" > <b>02 </b> MEET YOUR CREW</p>
-
                     <article>
-
-
                         <div className="crew-name">
-                            <p className="role">{ crew.role}</p>
-                            <p className="name">{ crew.name}</p>
+                            <p className="role">{crew.role}</p>
+                            <p className="name">{crew.name}</p>
 
                         </div>
 
-                        <p className="crew-member-info">  { crew.bio}
+                        <p className="crew-member-info">  {crew.bio}
                             and former NASA astsronaut. He launched into space for the third time as
                             commander of Crew Dragon Demo-2.
                         </p>
                     </article>
-              
+
                     <nav className="crew-nav">
-                        <div onClick={()=> setRole('Commander')} > </div>
-                        <div onClick={()=> setRole('Mission Specialist')} > </div>
-                        <div onClick={()=> setRole('Pilot')} > </div>
-                        <div onClick={()=> setRole('Flight Engineer')} > </div>
-                     
+                        <div onClick={() => setRole('Commander')} > </div>
+                        <div onClick={() => setRole('Mission Specialist')} > </div>
+                        <div onClick={() => setRole('Pilot')} > </div>
+                        <div onClick={() => setRole('Flight Engineer')} > </div>
+
                     </nav>
                 </section>
 
